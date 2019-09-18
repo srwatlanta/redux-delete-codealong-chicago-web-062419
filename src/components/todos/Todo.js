f4e9fb6f316364b3a16931f92bfbb368aaa6dd0d
srwatlanta <shawnrwheeler@gmail.com>
2019-09-18 15:07:04 -0500

    Done.

--- src/components/todos/Todo.js
+++ src/components/todos/Todo.js
@@ -1,5 +1,8 @@
 import React from 'react'
 
-const Todo = props => <li>{props.text}</li>
+
+const Todo = props => <li>{props.todo.text}<button onClick={()=>props.delete(props.todo.id)}>delete</button></li>
+
+
 
 export default Todo;
--- src/components/todos/TodosContainer.js
+++ src/components/todos/TodosContainer.js
@@ -4,7 +4,7 @@ import Todo from './Todo'
 
 class TodosContainer extends Component {
 
-  renderTodos = () => this.props.todos.map((todo, id) => <Todo key={id} text={todo} />)
+  renderTodos = () => this.props.todos.map((todo, id) => <Todo todo={todo} key={id} text={todo} delete={this.props.deleteTodo}/>)
 
   render() {
     return(
@@ -15,10 +15,15 @@ class TodosContainer extends Component {
   }
 };
 
-mapStateToProps = state => {
+const mapStateToProps = state => {
   return {
     todos: state.todos
   }
 }
+const mapDispatchToProps = dispatch => {
+  return {
+    deleteTodo: todo => dispatch({type: 'DELETE_TODO', payload: todo})
+  }
+}
 
-export default connect(mapStateToProps)(TodosContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(TodosContainer);
